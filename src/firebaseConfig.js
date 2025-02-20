4aa96c6e2ef12fecea0891983cacb418637e94a4
vin <vin@email>
2025-02-20 12:20:01 +0800

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-	apiKey:"AIzaSyDwML5MHO4FmSAwDHWWGNuS4JoIzu2ECS0",authDomain:"scarcely-calm-lark.firebaseapp.com",projectId:"scarcely-calm-lark",storageBucket:"scarcely-calm-lark.appspot.com",messagingSenderId:"761033708851",appId:"1:761033708851:web:fb54f41dc499d5a8ee1a46",measurementId:"G-LFXN33MRKC"
+	apiKey:"AIzaSyB6rUw_KY1UObdN61ni2YbdBG-M45nX7bQ",authDomain:"indie-hackers.firebaseapp.com",projectId:"indie-hackers",storageBucket:"indie-hackers.appspot.com",messagingSenderId:"761033708851",appId:"1:761033708851:web:fb54f41dc499d5a8ee1a46",measurementId:"G-LFXN33MRKC"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
---
+++ src/firebaseConfig.js.bak
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+	apiKey:"AIzaSyDwML5MHO4FmSAwDHWWGNuS4JoIzu2ECS0",authDomain:"scarcely-calm-lark.firebaseapp.com",projectId:"scarcely-calm-lark",storageBucket:"scarcely-calm-lark.appspot.com",messagingSenderId:"761033708851",appId:"1:761033708851:web:fb54f41dc499d5a8ee1a46",measurementId:"G-LFXN33MRKC"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp
